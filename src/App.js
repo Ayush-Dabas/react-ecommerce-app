998ca8bb8e260a4f31424d81be7aab8f80181b9e
Ayush-Dabas <aydbs23@gmail.com>
2023-08-22 17:55:23 +0530

    doing random shit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
-// import ProductDetail from "./components/ProductDetail";
-import ProductListing from "./components/ProductListing";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Cart from "./components/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom/cjs/react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <ProductListing />
-
-        {/* <Route path="/" element={<ProductListing />} /> */}
-        {/* <Route path="/Cart" element={<Cart />} /> */}
-        {/* <Link to="/Cart">Cart</Link> */}
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="App">
+        <ul></ul>
+      </div>
+    </Router>
   );
 };
 
