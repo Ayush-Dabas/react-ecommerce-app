3b7cb09ad33f28e9b2c89412e0b1fd444084829c
Ayush-Dabas <aydbs23@gmail.com>
2023-08-23 17:00:09 +0530

    changed some css

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import ProductDetail from "./ProductDetail";
+// import ProductDetail from "./ProductDetail";
 
 const Cart = () => {
   return (
     <>
       <h1>Cart here</h1>
-      <ProductDetail />
       <span>Total price: ₹price</span>
       <button>Proceed to payment</button>
     </>
